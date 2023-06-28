9f901d8e3de0d27968ab3111e9fcefebe4d3f6c4
Luan Freire <lunfreire@projectbox.pt>
2023-06-28 15:11:41 -0300

    atualização da porta para 8080

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-from flask import Flask, jsonify, make_response, request
-from flask_cors import CORS, cross_origin
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 
 
 @app.route('/', methods=['GET'])
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def getAPI():
-    return make_response(
-        jsonify("API está funcionando na Railway.")
-    )
+    return make_response(jsonify("API está funcionando na Railway."))
 
-app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
