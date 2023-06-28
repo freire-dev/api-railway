672cd2910acfb95496bc37a8add684e3f0ae0570
Luan Freire <lunfreire@projectbox.pt>
2023-06-28 15:33:44 -0300

    alteração do corpo do projeto.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -6,10 +6,13 @@ CORS(app)
 app.config['JSON_SORT_KEYS'] = False
 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/', methods=['GET'])
 def getAPI():
     return make_response(jsonify("API está funcionando na Railway."))
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=5000, debug=True)
